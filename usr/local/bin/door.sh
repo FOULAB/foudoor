586e1649c8f317c96d7bb67cb3f19f2d508fdd16
eigma <eigma@foulab.org>
2024-09-21 14:19:30 +0300

    Ensure latch gets closed even if user kills command.
    Vulnerabilty ID: FVE-2024-001
    
    Repro:
    $ ssh -t foudoor.lab /usr/local/bin/door.sh
    ^C
    <latch remains open indefinitely>
    
    Note -t is necessary to ensure the ^C is sent across and kill the
    door.sh command.
    
    Reported-by: abby
    Relayed-by: computerbomb

--- usr/local/bin/door.sh
+++ usr/local/bin/door.sh
@@ -9,6 +9,7 @@ TIMEOUT=10
 
 # SET gpio to output
 echo out > /sys/class/gpio/gpio50/direction 
+
+trap 'echo 0 > /sys/class/gpio/gpio50/value' EXIT
 echo 1 > /sys/class/gpio/gpio50/value 
 sleep $TIMEOUT
-echo 0 > /sys/class/gpio/gpio50/value 
