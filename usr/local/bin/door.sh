dbd812cece91bdd266863afd5a57aa38e72f829d
matildepark <matilde.park@pm.me>
2025-12-03 05:34:40 +0000

    door.sh: update for gpio interface in 6.12 kernel (#1)
    * door.sh: update for gpio interface in 6.12 kernel
    
    * door.sh: rewrite to retain trap so it boots you at the end of the timeout, instead of end of session
    
    * door.sh: use hold-period, remove trap
    
    The previous approach introduced a situation where the door could
    get stuck open or un-openable due to a dangling gpioset process
    holding the door open.
    
    Setting an explicit hold period avoids this issue, and in initial
    tests exiting early, seems to clean up as expected.

--- usr/local/bin/door.sh
+++ usr/local/bin/door.sh
@@ -5,11 +5,7 @@
 
 logger -t door.sh -p local0.notice FouDoor activated: $USER opened the door
 
-TIMEOUT=10
+TIMEOUT=5
 
-# SET gpio to output
-echo out > /sys/class/gpio/gpio50/direction 
-
-trap 'echo 0 > /sys/class/gpio/gpio50/value' EXIT
-echo 1 > /sys/class/gpio/gpio50/value 
+/usr/bin/gpioset --hold-period 5s -t 0 "P9_14 [ehrpwm1a]"=1
 sleep $TIMEOUT
